23cf47891b60528a5cddc7f2a257bb9284bad385
Luca <lucasimodev01@gmail.com>
2025-05-20 15:23:00 -0300

    fix/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), 
-        blog: resolve(__dirname, 'blog/blog.html'), 
-      },
-    },
-  },
 })
