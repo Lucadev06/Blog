440283b0a4edb408614e1cab5f1adf622e4cac0b
Luca <lucasimodev01@gmail.com>
2025-05-20 12:18:14 -0300

    feat: primer commit del blog en React

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import { Button, Box, Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1503264116251-35a269479413?auto=format&fit=crop&w=1950&q=80")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '2rem',
+        padding: '2rem',
+      }}
+    >
+      <Box
+        sx={{
+          width: '90%',
+          maxWidth: '25rem',
+          aspectRatio: '1 / 1',
+          clipPath:
+            'polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)',
+          backgroundColor: '#fff3db',
+          boxShadow: '0 12px 36px rgba(0,0,0,0.2)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src="./public/imagen-animada.png"
+          alt="Logo"
+          style={{
+            width: '60%',
+            objectFit: 'contain',
+          }}
+        />
+      </Box>
+
+      <Typography
+        variant="h5"
+        sx={{
+          color: 'white',
+          fontWeight: 'bold',
+          textShadow: '0px 0px 10px black',
+          textAlign: 'center',
+        }}
+      >
+        Luca Simonazzi - Desarrollador Frontend
+      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '1rem',
+          width: '100%',
+          maxWidth: '20rem',
+        }}
+      >
+        <Button variant="contained" fullWidth color="primary" component="a"
+          href="/IDENTIKIT/index.html"
+          target="_blank"
+          rel="noopener noreferrer">
+          IDENTIKIT
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          component="a"
+          href="/IDENTIKIT/blog/blog.html"
+          target="_blank"
+          fullWidth
+        >
+          BLOG
+        </Button>
+
+        <Button variant="contained" fullWidth color="success">
+          LINK 2
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default App
